8fde44cad5e821e454905e0d03c5b89d8df0e6d0
Sota Konnai <s1280068@u-aizu.ac.jp>
2021-07-02 12:25:50 +0900

    Won/lost message modified

--- DiceGame.py
+++ DiceGame.py
@@ -1,3 +1,6 @@
+name = input('What is your name?\n> ')
+print(f'Hello, {name}!')
+
 import random
 
 print('Rolling the dice...')
@@ -9,3 +12,8 @@ Dice.append(random.randint(1,6))
 print(f'Die 1: {Dice[0]}')
 print(f'Die 1: {Dice[1]}')
 print(f'Total value: {sum(Dice)}')
+
+if sum(Dice) > 7:
+    print(f'{name} won!')
+else:
+    print(f'{name} lost!')
