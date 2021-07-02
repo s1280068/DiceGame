4c9eb0f8b3a739197f2d00c2bfdbe6fed03471a9
Sota Konnai <s1280068@u-aizu.ac.jp>
2021-07-02 12:18:05 +0900

    Won/lost message added

---
+++ Judgement.py
@@ -0,0 +1,16 @@
+import random
+
+print('Rolling the dice...')
+
+Dice = []
+Dice.append(random.randint(1,6))
+Dice.append(random.randint(1,6))
+
+print(f'Die 1: {Dice[0]}')
+print(f'Die 1: {Dice[1]}')
+print(f'Total value: {sum(Dice)}')
+
+if sum(Dice) > 7:
+    print(f'You won!')
+else:
+    print(f'You lost!')
